bfcafc56fea94499ebc2df14ab80d80fb9f95832
Kim Duclos <kimduclos@gmail.com>
2020-06-08 14:44:51 -0600

    homepage layout aligned

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./header.scss";
 import Projects from "../projects/projects";
 import Skills from "../skills/skills";
 import ContactForm from "../contactForm/contactForm";
 
 const Header = () => {
   return (
-    <>
-      <h1>Kim Duclos</h1>
-      <h2>Web Development & Design</h2>
-      <Router>
-        <nav className="navbar">
-          <Link to="/">Projects</Link>
-          <Link to="/skills">Skills</Link>
-          <Link to="/contact">Contact</Link>
-           <hr />
-        </nav>
-        <div className="Header">
-          <Switch>
-            <Route exact path="/" component={Projects} />
-            <Route path="/skills" component={Skills} />
-            <Route path="/contact" component={ContactForm} />
-          </Switch>
+    <Router>
+      <div className="headerBlock">
+        <div className="titleBlock">
+          <div className="name">Kim Duclos</div>
+          <div className="subtitle">Web Development & Design</div>
         </div>
-      </Router>
-    </>
+        <div className="navBlock">
+          <nav className="navbar">
+            <Link to="/">Projects</Link>
+            <Link to="/skills">Skills</Link>
+            <Link to="/contact">Contact</Link>
+          </nav>
+          </div>
+      </div>
+          <div className="projectSection">
+            <Switch>
+              <Route exact path="/" component={Projects} />
+              <Route path="/skills" component={Skills} />
+              <Route path="/contact" component={ContactForm} />
+            </Switch>
+          </div>
+    </Router>
   );
 };
 
--- src/projects/projects.jsx
+++ src/projects/projects.jsx
@@ -5,10 +5,10 @@ import ProjectBox from "./projectBox.jsx";
 const Projects = () => {
   return (
     <div className="projects">
-      <ProjectBox caption="LiveSafe is an app that assists homebuyers in avoiding areas of natural disasters." />
-      <ProjectBox caption="FlexTogether pairs senior citizens with virtual workout partners based on schedule availability."/>
-      <ProjectBox caption="Lunchr connects donars with schools requesting monetary funding for student lunches."/>
-      <ProjectBox caption="Simpsom says is a fun app that accesses a database filled with thousands of Simpsons quotes." />
+        <ProjectBox caption="LiveSafe warns homebuyers of potential natural disasters that could affect safety and insurance rates." />
+        <ProjectBox caption="FlexTogether pairs seniors with workout partners based on schedule availability of both parties." />
+        <ProjectBox caption="Lunchr connects donars with schools requesting monetary funding for student lunches." />
+        <ProjectBox caption="Simpsom says is a fun app that accesses a database filled with thousands of Simpsons quotes." />
     </div>
   );
 };
