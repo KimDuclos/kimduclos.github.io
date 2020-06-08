0dd2832400be9e74f657812091ee5aa713b3502c
Kim Duclos <kimduclos@gmail.com>
2020-06-08 09:31:07 -0600

    skills section filled in

--- src/skills/skills.jsx
+++ src/skills/skills.jsx
@@ -4,12 +4,56 @@ import SkillSection from "./skillSection.jsx";
 const SkillsList = (props) => {
   return (
     <>
-      <SkillSection title="Graphic Design Software" />
-      <SkillSection title="Coding Editors" />
-      <SkillSection title="Website Builders" />
-      <SkillSection title="Coding Languages" />
+      <SkillSection
+        title="Graphic Design Software"
+        list=<ul>
+          <li>Adobe Photoshop CC</li>
+          <li>Adobe Illustrator CC></li>
+          <li>Adobe InDesign</li>
+          <li>Figma</li>
+          <li>Sketch</li>
+        </ul>
+      />
+      <SkillSection
+        title="Coding Editors"
+        list=<ul>
+          <li>Visual Studio Code</li>
+          <li>Atom</li>
+          <li>Notepad++</li>
+          <li>Adobe Dreamweaver CC</li>
+        </ul>
+      />
+      <SkillSection
+        title="Website Builders"
+        list=<ul>
+          <li>Wordpress</li>
+          <li>Squarespace</li>
+          <li>Wix</li>
+        </ul>
+      />
+      <SkillSection
+        title="Coding Languages"
+        list=<ul>
+          <li>HTML/HTML5</li>
+          <li>CSS/CSS3</li>
+          <li>Javascript</li>
+          <li>ReactJS</li>
+          <li>Python</li>
+        </ul>
+      />
       <hr></hr>
-      <SkillSection title="Areas of Exterpise" />
+      <SkillSection
+        title="Areas of Exterpise"
+        list=<ul>
+          <li>Website Design</li>
+          <li>UI/UX Design</li>
+          <li>Responsive Design</li>
+          <li>Website Maitenance & Updates</li>
+          <li>Brochure, Flyer & Pamphlet Design</li>
+          <li>Corporate Identity (logos, business cards & letterhead)</li>
+          <li>Image Manipulation</li>
+        </ul>
+      />
     </>
   );
 };
