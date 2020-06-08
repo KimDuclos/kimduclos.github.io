7d07e0bb7bce57b15ff3968ce159aceb14746b25
Kim Duclos <kimduclos@gmail.com>
2020-06-08 15:12:42 -0600

    color/font styling done on skills

--- src/skills/skillSection.jsx
+++ src/skills/skillSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./skillSection.scss";
 
 const SkillSection = (props) => {
   return (
--- src/skills/skills.jsx
+++ src/skills/skills.jsx
@@ -1,47 +1,50 @@
 import React from "react";
+import "./skills.scss";
 import SkillSection from "./skillSection.jsx";
 
 const SkillsList = (props) => {
   return (
     <>
-      <SkillSection
-        title="Graphic Design Software"
-        list=<ul>
-          <li>Adobe Photoshop CC</li>
-          <li>Adobe Illustrator CC></li>
-          <li>Adobe InDesign</li>
-          <li>Figma</li>
-          <li>Sketch</li>
-        </ul>
-      />
-      <SkillSection
-        title="Coding Editors"
-        list=<ul>
-          <li>Visual Studio Code</li>
-          <li>Atom</li>
-          <li>Notepad++</li>
-          <li>Adobe Dreamweaver CC</li>
-        </ul>
-      />
-      <SkillSection
-        title="Website Builders"
-        list=<ul>
-          <li>Wordpress</li>
-          <li>Squarespace</li>
-          <li>Wix</li>
-        </ul>
-      />
-      <SkillSection
-        title="Coding Languages"
-        list=<ul>
-          <li>HTML/HTML5</li>
-          <li>CSS/CSS3</li>
-          <li>Javascript</li>
-          <li>ReactJS</li>
-          <li>Python</li>
-        </ul>
-      />
-      <hr></hr>
+      <div className="skillsTop">
+        <SkillSection
+          title="Graphic Design Software"
+          list=<ul>
+            <li>Adobe Photoshop CC</li>
+            <li>Adobe Illustrator CC</li>
+            <li>Adobe InDesign</li>
+            <li>Figma</li>
+            <li>Sketch</li>
+          </ul>
+        />
+        <SkillSection
+          title="Coding Editors"
+          list=<ul>
+            <li>Visual Studio Code</li>
+            <li>Atom</li>
+            <li>Notepad++</li>
+            <li>Adobe Dreamweaver CC</li>
+          </ul>
+        />
+        <SkillSection
+          title="Website Builders"
+          list=<ul>
+            <li>Wordpress</li>
+            <li>Squarespace</li>
+            <li>Wix</li>
+          </ul>
+        />
+        <SkillSection
+          title="Coding Languages"
+          list=<ul>
+            <li>HTML/HTML5</li>
+            <li>CSS/CSS3</li>
+            <li>Javascript</li>
+            <li>ReactJS</li>
+            <li>Python</li>
+          </ul>
+        />
+      </div>
+      <hr />
       <SkillSection
         title="Areas of Exterpise"
         list=<ul>
