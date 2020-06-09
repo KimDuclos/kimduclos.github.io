6c0839938fc1d1cf52183dbbba012d4f2701dc1e
Kim Duclos <kimduclos@gmail.com>
2020-06-09 09:49:24 -0600

    skills page layout done

--- src/skills/skillSection.jsx
+++ src/skills/skillSection.jsx
@@ -3,10 +3,12 @@ import "./skillSection.scss";
 
 const SkillSection = (props) => {
   return (
-    <>
+    <div className="skillSection">
       <div className="title">{props.title}</div>
-      <div className="list"><ul>{props.list}</ul></div>
-    </>
+      <div className="list">
+        <ul>{props.list}</ul>
+      </div>
+    </div>
   );
 };
 
