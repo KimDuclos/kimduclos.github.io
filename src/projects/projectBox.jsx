b330859a268592b9c5584e231a533ea74611458c
KimDuclos <kimduclos@gmail.com>
2020-07-08 17:53:25 -0600

    project boxes filled and linked

--- src/projects/projectBox.jsx
+++ src/projects/projectBox.jsx
@@ -1,13 +1,26 @@
-import React from "react";
-import "./projectBox.scss";
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from 'react';
+import './projectBox.scss';
 
-const ProjectBox = (props) => {
-  return (
-    <div className="boxes">
-      <div className="projectBox"></div>
-      <div className="caption">{props.caption}</div>
-    </div>
-  );
+const ProjectBox = props => {
+	return (
+    <>
+		  <div className="boxes">
+          <div className="projectBox">
+          <a href={props.link} target="_blank">
+            <img 
+              src={props.image} 
+              alt={props.alt} 
+            />
+            </a>
+          </div>
+			<div className="caption">
+				{props.caption}
+			</div>
+      </div>   
+		</>
+	);
 };
 
 export default ProjectBox;
--- src/projects/projects.jsx
+++ src/projects/projects.jsx
@@ -7,10 +7,30 @@ const Projects = () => {
   return (
     <>
     <div className="projects">
-        <ProjectBox caption="LiveSafe warns homebuyers of potential natural disasters that could affect safety and insurance rates." />
-        <ProjectBox caption="FlexTogether pairs seniors with workout partners based on schedule availability of both parties." />
-        <ProjectBox caption="Lunchr connects donars with schools requesting monetary funding for student lunches." />
-        <ProjectBox caption="Simpson says is a fun app that accesses a database filled with thousands of Simpsons quotes." />
+        <ProjectBox 
+            image="/images/flex-together.PNG" 
+            alt="Flex Together Image"
+            caption="LiveSafe warns homebuyers of potential natural disasters that could affect safety and insurance rates." 
+            link="https://flex-together-max.netlify.com/"
+        />
+        <ProjectBox 
+            image="/images/lunchr.PNG" 
+            alt="Lunchr Image"
+            caption="FlexTogether pairs seniors with workout partners based on schedule availability of both parties." 
+            link="https://luncher-team.github.io/luncher-app-frank-UI/"
+        />
+        <ProjectBox 
+            image="/images/live-safe.PNG" 
+            alt="Live SafeImage"
+            caption="Lunchr connects donars with schools requesting monetary funding for student lunches." 
+            link="https://livesafe.services/"
+        />
+        <ProjectBox 
+            image="/images/simpson-says.PNG" 
+            alt="Simpson Says Image"
+            caption="Simpson says is a fun app that accesses a database filled with thousands of Simpsons quotes."
+            link="https://simpsonssays.netlify.app/login"
+        />
     </div>
     <Footer />
     </>
