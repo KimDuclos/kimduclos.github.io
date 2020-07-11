346ee82c1316379aa5ce3dd01523493a128cd816
KimDuclos <kimduclos@gmail.com>
2020-07-11 08:02:51 -0600

    Updates

---
+++ precache-manifest.96d38f01b8ccc552dea1d8b60ae13f79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8e345639d26ffdb04d6fd5f829434d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ed3adc7cd1244b4ca8b",
+    "url": "/static/css/main.3d25245b.chunk.css"
+  },
+  {
+    "revision": "83003c22ca619bb79b39",
+    "url": "/static/js/2.870fbaec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.870fbaec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed3adc7cd1244b4ca8b",
+    "url": "/static/js/main.110fff0c.chunk.js"
+  },
+  {
+    "revision": "fb6c455073af1f9ff275",
+    "url": "/static/js/runtime-main.74f710ee.js"
+  }
+]);
