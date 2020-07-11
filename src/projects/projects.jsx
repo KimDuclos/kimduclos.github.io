c722ad514ef3f7f7f7eadd0e26741b58c67d7f50
KimDuclos <kimduclos@gmail.com>
2020-07-11 07:56:17 -0600

    removed deployment scripts from package.json

--- src/projects/projects.jsx
+++ src/projects/projects.jsx
@@ -8,26 +8,26 @@ const Projects = () => {
     <>
     <div className="projects">
         <ProjectBox 
-            image="/images/flex-together.PNG" 
+            image="/images/flex-together.png" 
             alt="Flex Together Image"
             caption="FlexTogether pairs seniors with workout partners based on schedule availability of both parties." 
             link="https://flex-together-max.netlify.com/"
         />
         <ProjectBox 
-            image="/images/lunchr.PNG" 
+            image="/images/lunchr.png" 
             alt="Lunchr Image"
             caption="Lunchr connects donors with schools requesting monetary funding for student lunches." 
             link="https://luncher-team.github.io/luncher-app-frank-UI/"
         />
         <ProjectBox 
-            image="/images/live-safe.PNG" 
+            image="/images/live-safe.png" 
             alt="Live SafeImage"
             caption="LiveSafe warns homebuyers of potential natural disasters that could affect safety and insurance rates." 
 
             link="https://livesafe.services/"
         />
         <ProjectBox 
-            image="/images/simpson-says.PNG" 
+            image="/images/simpson-says.png" 
             alt="Simpson Says Image"
             caption="Simpson says is a fun app that accesses a database filled with thousands of Simpsons quotes."
             link="https://simpsonssays.netlify.app/login"
