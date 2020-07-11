011511344be77acb488998feb3a92154b6470d69
KimDuclos <kimduclos@gmail.com>
2020-07-11 13:56:59 -0600

    updated router links in header for gh-pages

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -25,9 +25,9 @@ const Header = () => {
 			</div>
 			<div className="projectSection">
 				<Switch>
-					<Route exact path="/" component={Projects} />
-					<Route exact path="/skills" component={Skills} />
-					<Route exact path="/contactForm" component={ContactForm} />
+					<Route Route path={process.env.PUBLIC_URL + "/"} component={Projects} />
+					<Route Route path={process.env.PUBLIC_URL + "/skills"} component={Skills} />
+					<Route Route path={process.env.PUBLIC_URL + "/contactForm"} component={ContactForm} />
 				</Switch>
 			</div>
 		</Router>
